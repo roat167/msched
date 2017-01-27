0502d17d13fbe384d8af94d477a4fc88d9612968
khemroat <khemroat@gmail.com>
2017-01-26 18:03:30 -0600

    update login and menu
    # Conflicts:
    #	src/main/java/edu/mum/swe/msched/SecurityConfig.java

--- src/main/java/edu/mum/swe/msched/service/UserService.java
+++ src/main/java/edu/mum/swe/msched/service/UserService.java
@@ -2,46 +2,13 @@ package edu.mum.swe.msched.service;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-import javax.transaction.Transactional;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import edu.mum.swe.msched.dao.UserDao;
 import edu.mum.swe.msched.domain.User;
 
-@Service
-@Transactional
-public class UserService {
-	@Resource
-	UserDao userDao;
-
-	public void remove(Long id) {
-		userDao.delete(id);
-	}
-
-	public void save(User user) {
-		userDao.save(user);
-	}
-
-	public User findUserById(Long id) {
-		return userDao.findOne(id);
-	}
-
-	public void saveUser(User user) {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String hashedPassword = passwordEncoder.encode(user.getPassword());
-		user.setPassword(hashedPassword);
-		userDao.save(user);
-	}
-
-	public User findByUsername(String username) {
-		return userDao.findByUsername(username);
-	}
-
-	public List<User> getAllUsers() {
-		return userDao.findAll();
-	}
-
+public interface UserService {
+	User findByUsername(String username);
+	List<User> getAllUsers();
+	void saveUser(User user);
+	User findUserById(Long id);
+	void save(User user);
+	void remove(Long id);
 }
