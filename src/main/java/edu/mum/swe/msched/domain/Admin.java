86192aa7973a481d48b2187ebf463375e86b8fa9
Mainali <mainalipukar@gmail.com>
2017-01-28 14:27:24 -0600

    add admin entity

---
+++ src/main/java/edu/mum/swe/msched/domain/Admin.java
@@ -0,0 +1,28 @@
+package edu.mum.swe.msched.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Admin implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long AdminId;
+	
+	private Long userId;
+	
+	
+	
+
+}
