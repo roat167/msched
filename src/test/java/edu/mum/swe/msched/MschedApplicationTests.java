479aae8f5db2fa8e24dd2b760ddc19f3a27b2ec0
khemroat <khemroat@gmail.com>
2017-06-09 15:35:18 -0700

    - update packaging from war to jar, clean code in test file

--- src/test/java/edu/mum/swe/msched/MschedApplicationTests.java
+++ src/test/java/edu/mum/swe/msched/MschedApplicationTests.java
@@ -5,9 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import edu.mum.swe.msched.service.UserService;
-import edu.mum.swe.msched.service.impl.UserServiceImpl;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MschedApplicationTests {
